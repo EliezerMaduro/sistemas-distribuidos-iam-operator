8c26a2f8c7fd390e619b70f3a929bd285ba817c9
Eliezer Maduro <eliezer.maduro@gmail.com>
2024-02-17 15:55:23 -0500

    Initial structure

---
+++ documents/src/main/kotlin/com/iam/operator/micarpeta/MicarpetaApplication.kt
@@ -0,0 +1,11 @@
+package com.iam.operator.micarpeta
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MicarpetaApplication
+
+fun main(args: Array<String>) {
+	runApplication<MicarpetaApplication>(*args)
+}
---
+++ documents/src/test/kotlin/com/iam/operator/micarpeta/MicarpetaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.iam.operator.micarpeta
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MicarpetaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
