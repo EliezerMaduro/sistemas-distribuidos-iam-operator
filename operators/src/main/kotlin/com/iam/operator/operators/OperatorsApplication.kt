bf6e5ec2bde671890371574c2bd26cf534a0de5e
Eliezer Maduro <eliezer.maduro@gmail.com>
2024-02-17 15:59:37 -0500

    Add all the initial services structure

---
+++ operators/src/main/kotlin/com/iam/operator/operators/OperatorsApplication.kt
@@ -0,0 +1,11 @@
+package com.iam.operator.operators
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OperatorsApplication
+
+fun main(args: Array<String>) {
+	runApplication<OperatorsApplication>(*args)
+}
---
+++ operators/src/test/kotlin/com/iam/operator/operators/OperatorsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.iam.operator.operators
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OperatorsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
